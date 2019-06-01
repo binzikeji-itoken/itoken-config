ffcc1c8f8632e9262048c6a086b7624e194bee82
ShuaiBin <3166688833@qq.com>
2019-06-01 23:32:02 +0800

    init

---
+++ src/main/java/com/binzikeji/itoken/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.binzikeji.itoken.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description
+ * @Author Bin
+ * @Date 2019/4/3 9:52
+ **/
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
